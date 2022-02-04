b9dfa59f33fbd6a2f825b47a63d3c349ef447e97
Jan Emrich <jan.emrich@igd.fraunhofer.de>
2022-02-04 17:04:41 +0100

    added denstereo to dataset_params

--- bop_toolkit_lib/dataset_params.py
+++ bop_toolkit_lib/dataset_params.py
@@ -85,6 +85,8 @@ def get_model_params(datasets_path, dataset_name, model_type=None):
     'hbs': [1, 3, 4, 8, 9, 10, 12, 15, 17, 18, 19, 22, 23, 29, 32, 33],
     'hb': list(range(1, 34)),  # Full HB dataset.
     'ycbv': list(range(1, 22)),
+    'denstereo-left': list(range(1, 22)),
+    'denstereo-right': list(range(1, 22)),
     'hope': list(range(1, 29)),
   }[dataset_name]
 
@@ -103,6 +105,8 @@ def get_model_params(datasets_path, dataset_name, model_type=None):
     'hbs': [10, 12, 18, 29],
     'hb': [6, 10, 11, 12, 13, 14, 18, 24, 29],
     'ycbv': [1, 13, 14, 16, 18, 19, 20, 21],
+    'denstereo-left': [1, 13, 14, 16, 18, 19, 20, 21],
+    'denstereo-right': [1, 13, 14, 16, 18, 19, 20, 21],
     'hope': None,  # Not defined yet.
   }[dataset_name]
 
@@ -359,6 +363,51 @@ def get_split_params(datasets_path, dataset_name, split, split_type=None):
       p['azimuth_range'] = (0, 2 * math.pi)
       p['elev_range'] = (-1.2788, 1.1291)  # (-73.27, 64.69) [deg].
 
+  # Denstereo
+  elif dataset_name == 'denstereo-left':
+    if split == 'train' and split_type is None:
+      split_type = 'real'
+
+    if split == 'train':
+      p['scene_ids'] = {
+        'real': list(range(48)) + list(range(60, 92)),
+        'pbr': None,  # Use function get_present_scene_ids().
+        'synt': list(range(80))
+      }[split_type]
+    elif split == 'test':
+      # p['scene_ids'] = list(range(48, 60))
+      return NotImplementedError
+
+    p['im_size'] = (640, 480)
+
+    if split == 'test':
+      p['depth_range'] = (612.92, 1243.59)
+      # p['azimuth_range'] = (0, 2 * math.pi)
+      # p['elev_range'] = (-1.2788, 1.1291)  # (-73.27, 64.69) [deg].
+
+  # Denstereo
+  elif dataset_name == 'denstereo-right':
+    if split == 'train' and split_type is None:
+      split_type = 'real'
+
+    if split == 'train':
+      p['scene_ids'] = {
+        'real': list(range(48)) + list(range(60, 92)),
+        'pbr': None,  # Use function get_present_scene_ids().
+        'synt': list(range(80))
+      }[split_type]
+    elif split == 'test':
+      # p['scene_ids'] = list(range(48, 60))
+      return NotImplementedError
+
+    p['im_size'] = (640, 480)
+
+    if split == 'test':
+      p['depth_range'] = (612.92, 1243.59)
+      # p['azimuth_range'] = (0, 2 * math.pi)
+      # p['elev_range'] = (-1.2788, 1.1291)  # (-73.27, 64.69) [deg].
+
+
   # HOPE.
   elif dataset_name == 'hope':
     p['scene_ids'] = {
@@ -434,8 +483,10 @@ def get_present_scene_ids(dp_split):
   :param dp_split: Path to a folder with datasets.
   :return: List with scene ID's.
   """
+  print('split path', dp_split['split_path'])
   scene_dirs = [d for d in glob.glob(os.path.join(dp_split['split_path'], '*'))
                 if os.path.isdir(d)]
   scene_ids = [int(os.path.basename(scene_dir)) for scene_dir in scene_dirs]
+  print('scene_dir', scene_dirs)
   scene_ids = sorted(scene_ids)
   return scene_ids
